ddf1f07cacd55c7abb3aea929bb5ece21c1e7b65
Nikita <mironov.nik88@gmail.com>
2021-11-05 18:47:04 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname,'secret-folder'), (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.forEach(file => {
+        fs.stat(path.join(__dirname,'secret-folder',file), (err, stats) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            if(stats.isFile()) {
+                const extname = path.extname(file) 
+                const res = path.basename(file, extname) + " - " + extname.slice(1) + " - " + stats.size;
+                console.log(res);
+            }
+        })
+    });
+})
