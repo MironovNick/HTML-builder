fcdd50ec21108c4e6cac47bf5dee65cab1ec6c14
Nikita <mironov.nik88@gmail.com>
2021-11-07 17:00:26 +0300

    feat: add 04-copy-directiry code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const {readdir, stat, unlink, mkdir, copyFile} = require('fs/promises');
 
+const indir = path.join(__dirname,'files');
+const outdir = path.join(__dirname,'files-copy');
+
+async function copyDirf() {
+             try {
+                try {
+                    const stats = await stat(outdir);
+                    const files = await readdir(outdir);
+                    for (const file of files)
+                        await unlink(path.join(outdir,file));
+                } catch (err) {
+                    if(err.code === 'ENOENT')
+                         mkdir(outdir, { recursive: true });
+                    else
+                        throw err;
+                }
+                
+                const files = await readdir(indir);
+                for (const file of files)
+                    await copyFile(path.join(indir,file), path.join(outdir,file));
+            } catch (err) {
+                console.error(err);
+            }
+}
+
+copyDirf();
